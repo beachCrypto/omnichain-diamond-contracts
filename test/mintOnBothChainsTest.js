99197d8c6bf04a53429832e86236af7a23be1b41
Beach Crypto <beachcrypto1@gmail.com>
2023-05-14 23:24:20 -0700

    Removing more comments

--- test/mintOnBothChainsTest.js
+++ test/mintOnBothChainsTest.js
@@ -36,13 +36,6 @@ describe('sendFrom()', async () => {
         lzEndpointMockA = await LZEndpointMock.deploy(chainId_A);
         lzEndpointMockB = await LZEndpointMock.deploy(chainId_B);
 
-        // console.log('lzEndpointMockA', lzEndpointMockA.address);
-        // console.log('lzEndpointMockB', lzEndpointMockB.address);
-        // lzEndpointMockA 0x5FbDB2315678afecb367f032d93F642f64180aa3
-        // lzEndpointMockB 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
-        // A will be Goerli
-        // B will be Polygon
-
         diamondAddressA = await deployDiamondA();
         diamondAddressB = await deployDiamondB();
 
