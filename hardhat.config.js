be34c189624269711a43caed01cd2253ab7b1a51
Beach Crypto <beachcrypto1@gmail.com>
2023-05-23 14:12:09 -0700

    Adding hardhat verify and wiring up the ls endpoints for testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
+require('@nomicfoundation/hardhat-verify');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
